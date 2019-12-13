296b1f5126622c1f3884ca318968cc84d47883c4
xiaobai <18756096010@163.com>
2019-12-13 19:53:53 +0800

    获取总资产接口

--- ex-fund-account/src/main/java/com/hp/sh/expv3/fund/extension/service/CapitalAccountService.java
+++ ex-fund-account/src/main/java/com/hp/sh/expv3/fund/extension/service/CapitalAccountService.java
@@ -1,7 +1,12 @@
 package com.hp.sh.expv3.fund.extension.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author BaiLiJun  on 2019/12/13
  */
+@Service
+@Transactional(rollbackFor=Exception.class)
 public class CapitalAccountService {
 }
