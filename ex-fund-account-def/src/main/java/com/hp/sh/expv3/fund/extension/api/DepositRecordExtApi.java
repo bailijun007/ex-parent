f1a4f0da8c22ea78a40215d8de5994062b54a002
xiaobai <18756096010@163.com>
2019-12-20 15:08:53 +0800

    代码优化

--- ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/extension/api/DepositRecordExtApi.java
+++ ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/extension/api/DepositRecordExtApi.java
@@ -1,5 +1,6 @@
 package com.hp.sh.expv3.fund.extension.api;
 
+import com.gitee.hupadev.base.api.PageResult;
 import com.hp.sh.expv3.fund.extension.vo.DepositRecordHistoryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -37,14 +38,12 @@ public interface DepositRecordExtApi {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "用户id", example = "1", required = false),
             @ApiImplicitParam(name = "asset", value = "资产类型", example = "BTC", required = false),
-            @ApiImplicitParam(name = "queryId", value = "充币记录表主键编号", example = "1",required = false),
-            @ApiImplicitParam(name = "pageStatus", value = "翻页状态：-1：上一页，1：下一页", example = "1",required = true),
+            @ApiImplicitParam(name = "pageNo", value = "当前页", example = "1", required = true),
             @ApiImplicitParam(name = "pageSize", value = "页行数", example = "10", required = true)
     })
     @GetMapping(value = "/api/extension/account/deposit/queryHistory")
-    public List<DepositRecordHistoryVo> queryAllUserHistory(@RequestParam(value = "userId") Long userId, @RequestParam(value = "asset", required = false) String asset,
-                                                     @RequestParam(value = "queryId", required = false) Long queryId, @RequestParam(value = "pageSize") Integer pageSize,
-                                                     @RequestParam(value = "pageStatus") Integer pageStatus);
+    public PageResult<DepositRecordHistoryVo> queryAllUserHistory(@RequestParam(value = "userId") Long userId, @RequestParam(value = "asset", required = false) String asset,
+                                                                  @RequestParam(value = "pageNo", required = true) Integer pageNo, @RequestParam(value = "pageSize") Integer pageSize);
 
 
 }
--- ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/extension/api/WithdrawalRecordExtApi.java
+++ ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/extension/api/WithdrawalRecordExtApi.java
@@ -1,5 +1,6 @@
 package com.hp.sh.expv3.fund.extension.api;
 
+import com.gitee.hupadev.base.api.PageResult;
 import com.hp.sh.expv3.fund.extension.vo.DepositRecordHistoryVo;
 import com.hp.sh.expv3.fund.extension.vo.WithdrawalRecordVo;
 import io.swagger.annotations.Api;
@@ -24,8 +25,8 @@ public interface WithdrawalRecordExtApi {
     @ApiOperation("获取充币历史")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "用户id", example = "2", required = true),
-            @ApiImplicitParam(name = "asset", value = "资产类型", example = "ETH",required = true),
-            @ApiImplicitParam(name = "queryId", value = "充币记录表主键编号", example = "1",required = false),
+            @ApiImplicitParam(name = "asset", value = "资产类型", example = "ETH", required = true),
+            @ApiImplicitParam(name = "queryId", value = "充币记录表主键编号", example = "1", required = false),
             @ApiImplicitParam(name = "pageStatus", value = "翻页状态：-1：上一页，1：下一页", example = "1"),
             @ApiImplicitParam(name = "pageSize", value = "页行数", example = "10", required = true)
     })
@@ -38,15 +39,13 @@ public interface WithdrawalRecordExtApi {
     @ApiOperation("查询所有用户充币历史 后台admin专用")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "用户id", example = "2", required = false),
-            @ApiImplicitParam(name = "asset", value = "资产类型", example = "ETH",required = false),
-            @ApiImplicitParam(name = "queryId", value = "充币记录表主键编号", example = "1",required = false),
-            @ApiImplicitParam(name = "pageStatus", value = "翻页状态：-1：上一页，1：下一页", example = "1"),
+            @ApiImplicitParam(name = "asset", value = "资产类型", example = "ETH", required = false),
+            @ApiImplicitParam(name = "pageNo", value = "当前页", example = "1", required = true),
             @ApiImplicitParam(name = "pageSize", value = "页行数", example = "10", required = true)
     })
     @GetMapping(value = "/api/extension/account/withdrawal/queryHistory")
-    public List<WithdrawalRecordVo> queryAllUserHistory(@RequestParam(value = "userId",required = false) Long userId, @RequestParam(value = "asset",required = false) String asset,
-                                                 @RequestParam(value = "queryId", required = false) Long queryId, @RequestParam(value = "pageSize") Integer pageSize,
-                                                 @RequestParam(value = "pageStatus") Integer pageStatus);
+    public PageResult<WithdrawalRecordVo> queryAllUserHistory(@RequestParam(value = "userId", required = false) Long userId, @RequestParam(value = "asset", required = false) String asset,
+                                                              @RequestParam(value = "pageNo", required = true) Integer pageNo, @RequestParam(value = "pageSize") Integer pageSize);
 
 
 }
