02047cd41d56e310d47f7711ec42617519fa1d1d
xiaobai <18756096010@163.com>
2019-12-23 17:56:45 +0800

    add 获取当前用户历史委托 和 获取当前用户所有委托 接口

---
+++ ex-pc-extend-def/src/main/java/com/hp/sh/expv3/pc/extension/vo/UserOrderVo.java
@@ -0,0 +1,188 @@
+package com.hp.sh.expv3.pc.extension.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @author BaiLiJun  on 2019/12/23
+ */
+public class UserOrderVo implements Serializable {
+    @ApiModelProperty("id")
+    private Long id;
+
+    @ApiModelProperty("委托状态")
+    private Integer status;
+
+    @ApiModelProperty("手续费")
+    private BigDecimal fee;
+
+    @ApiModelProperty("委托价")
+    private BigDecimal price;
+
+    @ApiModelProperty("量")
+    private BigDecimal qty;
+
+    @ApiModelProperty("是否：1-多仓，0-空仓")
+    private Integer longFlag;
+
+    @ApiModelProperty("杠杆")
+    private BigDecimal leverage;
+
+    @ApiModelProperty("资产")
+    private String asset;
+
+    @ApiModelProperty("交易对")
+    private String symol;
+
+    @ApiModelProperty("委托创建时间")
+    private Long ctime;
+
+    @ApiModelProperty("平均价")
+    private BigDecimal avgPrice;
+
+    @ApiModelProperty("已成交量")
+    private BigDecimal filledQty;
+
+    @ApiModelProperty("是否:1-平仓,0-开")
+    private BigDecimal closeFlag;
+
+    @ApiModelProperty("成交比例")
+    private BigDecimal tradeRatio;
+
+    @ApiModelProperty("委托保证金")
+    private BigDecimal orderMargin;
+
+    @ApiModelProperty("委托类型")
+    private Integer orderType;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getQty() {
+        return qty;
+    }
+
+    public void setQty(BigDecimal qty) {
+        this.qty = qty;
+    }
+
+    public Integer getLongFlag() {
+        return longFlag;
+    }
+
+    public void setLongFlag(Integer longFlag) {
+        this.longFlag = longFlag;
+    }
+
+    public BigDecimal getLeverage() {
+        return leverage;
+    }
+
+    public void setLeverage(BigDecimal leverage) {
+        this.leverage = leverage;
+    }
+
+    public String getAsset() {
+        return asset;
+    }
+
+    public void setAsset(String asset) {
+        this.asset = asset;
+    }
+
+    public String getSymol() {
+        return symol;
+    }
+
+    public void setSymol(String symol) {
+        this.symol = symol;
+    }
+
+    public Long getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(Long ctime) {
+        this.ctime = ctime;
+    }
+
+    public BigDecimal getAvgPrice() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(BigDecimal avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+
+    public BigDecimal getFilledQty() {
+        return filledQty;
+    }
+
+    public void setFilledQty(BigDecimal filledQty) {
+        this.filledQty = filledQty;
+    }
+
+    public BigDecimal getCloseFlag() {
+        return closeFlag;
+    }
+
+    public void setCloseFlag(BigDecimal closeFlag) {
+        this.closeFlag = closeFlag;
+    }
+
+    public BigDecimal getTradeRatio() {
+        return tradeRatio;
+    }
+
+    public void setTradeRatio(BigDecimal tradeRatio) {
+        this.tradeRatio = tradeRatio;
+    }
+
+    public BigDecimal getOrderMargin() {
+        return orderMargin;
+    }
+
+    public void setOrderMargin(BigDecimal orderMargin) {
+        this.orderMargin = orderMargin;
+    }
+
+    public Integer getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(Integer orderType) {
+        this.orderType = orderType;
+    }
+}
---
+++ ex-pc-extend/src/main/java/com/hp/sh/expv3/pc/extension/api/PcOrderExtendApiAction.java
@@ -0,0 +1,106 @@
+package com.hp.sh.expv3.pc.extension.api;
+
+import com.hp.sh.expv3.commons.exception.ExException;
+import com.hp.sh.expv3.pc.extension.constant.PcPositionErrorCode;
+import com.hp.sh.expv3.pc.extension.service.PcOrderExtendService;
+import com.hp.sh.expv3.pc.extension.service.PcPositionExtendService;
+import com.hp.sh.expv3.pc.extension.service.impl.PcAccountExtendServiceImpl;
+import com.hp.sh.expv3.pc.extension.vo.UserOrderVo;
+import com.hp.sh.expv3.pc.extension.vo.PcOrderVo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author BaiLiJun  on 2019/12/23
+ */
+@RestController
+public class PcOrderExtendApiAction implements PcOrderExtendApi {
+    @Autowired
+    private PcAccountExtendServiceImpl pcAccountExtendService;
+
+    @Autowired
+    private PcOrderExtendService pcOrderExtendService;
+
+    @Autowired
+    private PcPositionExtendService pcPositionExtendService;
+
+    @Override
+    public List<UserOrderVo> query(Long userId, String asset, String symbol, Integer orderType, Integer longFlag, Integer closeFlag, Integer currentPage, Integer pageSize) {
+        if (StringUtils.isEmpty(asset) || StringUtils.isEmpty(symbol) || null == userId || currentPage == null || pageSize == null) {
+            throw new ExException(PcPositionErrorCode.PARAM_EMPTY);
+        }
+        List<UserOrderVo> result = new ArrayList<>();
+        List<PcOrderVo> list = pcOrderExtendService.findCurrentUserOrder(userId, asset, symbol, orderType, longFlag, closeFlag);
+        if (!CollectionUtils.isEmpty(list)) {
+            getOrderList(currentPage, pageSize, result, list);
+        }
+        return result;
+    }
+
+    private void getOrderVo(List<UserOrderVo> result, PcOrderVo orderVo) {
+        UserOrderVo vo = new UserOrderVo();
+        BeanUtils.copyProperties(orderVo, vo);
+        vo.setFee(orderVo.getFeeCost());
+        vo.setQty(orderVo.getVolume());
+        vo.setLongFlag(orderVo.getLongFlag());
+        Date created = orderVo.getCreated();
+        if (null != created) {
+            vo.setCtime(created.getTime());
+        }
+        //平均价 暂时写死，后期掉老王接口
+        vo.setAvgPrice(BigDecimal.ZERO);
+        vo.setFilledQty(orderVo.getFilledVolume());
+        vo.setCloseFlag(orderVo.getCloseFlag());
+        vo.setTradeRatio(orderVo.getFilledVolume().divide(orderVo.getVolume()));
+        vo.setOrderType(orderVo.getOrderType());
+        result.add(vo);
+    }
+
+    @Override
+    public List<UserOrderVo> queryHistory(Long userId, String asset, String symbol, Integer orderType, Integer longFlag, Integer closeFlag, Integer currentPage, Integer pageSize, Long lastOrderId, Integer nextPage) {
+        if (StringUtils.isEmpty(asset) || StringUtils.isEmpty(symbol) || null == userId || currentPage == null || pageSize == null || nextPage == null) {
+            throw new ExException(PcPositionErrorCode.PARAM_EMPTY);
+        }
+        List<UserOrderVo> result = new ArrayList<>();
+        List<PcOrderVo> list = pcOrderExtendService.queryHistory(userId, asset, symbol, orderType, longFlag, closeFlag, lastOrderId, currentPage, pageSize, nextPage);
+        if (!CollectionUtils.isEmpty(list)) {
+            for (PcOrderVo orderVo : list) {
+                getOrderVo(result, orderVo);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<UserOrderVo> queryAll(Long userId, String asset, String symbol, Integer status, Integer longFlag, Integer closeFlag, Integer currentPage, Integer pageSize) {
+        if (StringUtils.isEmpty(asset) || StringUtils.isEmpty(symbol) || null == userId || currentPage == null || pageSize == null) {
+            throw new ExException(PcPositionErrorCode.PARAM_EMPTY);
+        }
+        List<UserOrderVo> result = new ArrayList<>();
+        List<PcOrderVo> list = pcOrderExtendService.queryAll(userId, asset, symbol, status, longFlag, closeFlag);
+        if (!CollectionUtils.isEmpty(list)) {
+            getOrderList(currentPage, pageSize, result, list);
+        }
+
+        return result;
+    }
+
+    private void getOrderList(Integer currentPage, Integer pageSize, List<UserOrderVo> result, List<PcOrderVo> list) {
+        List<PcOrderVo> orderVos = list.stream().skip(pageSize * (currentPage - 1))
+                .limit(pageSize)
+                .collect(Collectors.toList());
+
+        for (PcOrderVo orderVo : orderVos) {
+            getOrderVo(result, orderVo);
+        }
+    }
+}
