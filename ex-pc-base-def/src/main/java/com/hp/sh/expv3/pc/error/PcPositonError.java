3beb836f47c80258470f792547bc3e756bf0a36c
wg <java_arch@163.com>
2020-01-17 15:09:36 +0800

    最大可减少保证金

--- ex-pc-base-def/src/main/java/com/hp/sh/expv3/pc/error/PcPositonError.java
+++ ex-pc-base-def/src/main/java/com/hp/sh/expv3/pc/error/PcPositonError.java
@@ -13,7 +13,7 @@ public class PcPositonError extends ErrorCode {
 
 	public static final PcPositonError POS_NOT_ENOUGH = new PcPositonError(-13101, "超过了可平仓位!");
 	public static final PcPositonError HAVE_ACTIVE_ORDER = new PcPositonError(-13102, "当前存在挂单，不能修改杠杆");
-	public static final PcPositonError NO_MORE_MARGIN = new PcPositonError(-13103, "保证金余额不足");
+	public static final PcPositonError NO_MORE_MARGIN = new PcPositonError(-13103, "超过了最多仓位可减少保证金");
 	
 	public static final PcPositonError PARAM_GT_MAX_LEVERAGE = new PcPositonError(-13104, "超过最大杠杆!");
 
--- ex-pc-base/src/main/java/com/hp/sh/expv3/pc/api/PcPostionApiAction.java
+++ ex-pc-base/src/main/java/com/hp/sh/expv3/pc/api/PcPostionApiAction.java
@@ -5,27 +5,37 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hp.sh.expv3.pc.module.position.entity.PcPosition;
+import com.hp.sh.expv3.pc.module.position.service.PcPositionDataService;
 import com.hp.sh.expv3.pc.module.position.service.PcPositionMarginService;
 
 @RestController
 public class PcPostionApiAction implements PcPostionApi {
 	
 	@Autowired
-	private PcPositionMarginService pcPositionService;
+	private PcPositionMarginService positionMarginService;
+	
+	@Autowired
+	private PcPositionDataService positionDataService;
 	
 	//调整保证金 加&减
 	public void changeMargin(Long userId, String asset, String symbol, Integer longFlag, Integer optType, BigDecimal amount){
-		pcPositionService.changeMargin(userId, asset, symbol, longFlag, optType, amount);
+		positionMarginService.changeMargin(userId, asset, symbol, longFlag, optType, amount);
 	}
 	
 	public boolean changeLeverage(Long userId, String asset, String symbol, Integer marginMode, Integer longFlag, BigDecimal leverage){
-		return pcPositionService.changeLeverage(userId, asset, symbol, marginMode, longFlag, leverage);
+		return positionMarginService.changeLeverage(userId, asset, symbol, marginMode, longFlag, leverage);
 	}
 	
 	//开关自动追加保证金
 	@Override
 	public boolean setAutoAddFlag(long userId, String asset, String symbol, Integer longFlag, Integer autoAddFlag){
-		return pcPositionService.setAutoAddFlag(userId, asset, symbol, longFlag, autoAddFlag);
+		return positionMarginService.setAutoAddFlag(userId, asset, symbol, longFlag, autoAddFlag);
 	}
 	
+	public String getCurrentPosition(long userId, String asset, String symbol, Integer longFlag){
+		PcPosition pos = positionDataService.getCurrentPosition(userId, asset, symbol, longFlag);
+		
+		return null;
+	}
 }
