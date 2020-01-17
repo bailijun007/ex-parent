86b68c6d7f686e4cbe1560c975ae487d5cfb4044
wg <java_arch@163.com>
2020-01-17 22:14:52 +0800

    CONFIG

--- ex-fund-account/src/main/java/com/hp/sh/expv3/config/db/DataSourceConfig.java
+++ ex-fund-account/src/main/java/com/hp/sh/expv3/config/db/DataSourceConfig.java
@@ -1,11 +1,7 @@
 package com.hp.sh.expv3.config.db;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +15,13 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableTransactionManagement
 public class DataSourceConfig {
 	
+//	@Order(1)
+//	public String  test(){
+//		
+//		return null;
+//	}
 
+	@Primary
 	@Order(1)
 	@Bean("primaryDataSource")
 	@ConfigurationProperties(prefix = "spring.datasource.hikari.primary")
@@ -27,32 +29,5 @@ public class DataSourceConfig {
 		HikariDataSource ds = new HikariDataSource();
 		return ds;
 	}
-
-//	@Order(2)
-//	@Bean("secondDataSource")
-//	@ConfigurationProperties(prefix = "spring.datasource.hikari.second")
-//	public DataSource secondDataSource() {
-//		HikariDataSource ds = new HikariDataSource();
-//		return ds;
-//	}
-//	
-//	
-//	@Primary
-//	@Order(3)
-//	@Bean("shardingDataSource")
-//	public DataSource shardingDataSource(@Qualifier("primaryDataSource") DataSource primaryDataSource, List<DataSource> dsList) throws SQLException {
-//		// 配置分片规则
-//		FunShardingBuilder builder = new FunShardingBuilder();
-//		builder.setDsList(dsList);
-//		builder.addTable("fund_account");
-//		builder.addTable("fund_account_record");
-//		builder.addTable("fund_transfer");
-//		builder.addTable("deposit_addr");
-//		builder.addTable("deposit_record");
-//		builder.addTable("withdrawal_addr");
-//		builder.addTable("withdrawal_record");
-//		builder.addTable("c2c_order");
-//		return builder.build();
-//	}
 
 }
