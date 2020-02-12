60a6689781f76d44333f89a9a73bbcc0d767c1cb
xiaobai <18756096010@163.com>
2020-02-12 18:00:19 +0800

    bb

---
+++ ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/service/impl/BbAccountServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hp.sh.expv3.bb.extension.service.impl;
+
+import com.hp.sh.expv3.bb.extension.service.BbAccountService;
+
+/**
+ * @author BaiLiJun  on 2020/2/12
+ */
+public class BbAccountServiceImpl implements BbAccountService {
+
+
+
+
+}
