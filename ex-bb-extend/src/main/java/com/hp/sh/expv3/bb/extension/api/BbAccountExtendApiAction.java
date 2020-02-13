c41520bdff2494bbcf56adb4684b09a0d8dfc69c
xiaobai <18756096010@163.com>
2020-02-13 22:59:10 +0800

    bbAccount

--- ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/api/BbAccountExtendApiAction.java
+++ ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/api/BbAccountExtendApiAction.java
@@ -1,16 +1,42 @@
 package com.hp.sh.expv3.bb.extension.api;
 
+import com.hp.sh.expv3.bb.extension.service.BbAccountExtService;
+import com.hp.sh.expv3.bb.extension.vo.BbAccountVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * @author BaiLiJun  on 2020/2/12
  */
 @RestController
-public class BbAccountExtendApiAction implements BbAccountExtendApi{
+public class BbAccountExtendApiAction implements BbAccountExtendApi {
 
+    @Autowired
+    private BbAccountExtService bbAccountExtService;
 
     @Override
-    public int createBBAccount(Long userId, String asset) {
-        return 0;
+    public void createBBAccount(Long userId, String asset) {
+        bbAccountExtService.createBBAccount(userId, asset);
+    }
+
+    @Override
+    public Boolean bbAccountExist(Long userId, String asset) {
+        return bbAccountExtService.bbAccountExist(userId, asset);
+    }
+
+    @Override
+    public BbAccountVo getBBAccount(Long userId, String asset) {
+        return bbAccountExtService.getBBAccount(userId, asset);
+    }
+
+    @Override
+    public BigDecimal getBalance(Long userId, String asset) {
+        BbAccountVo bbAccount = bbAccountExtService.getBBAccount(userId, asset);
+        if (bbAccount != null) {
+            return bbAccount.getBalance();
+        }
+        return null;
     }
 }
---
+++ ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/service/impl/BbAccountExtServiceImpl.java
@@ -0,0 +1,63 @@
+package com.hp.sh.expv3.bb.extension.service.impl;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hp.sh.expv3.bb.extension.dao.BbAccountExtMapper;
+import com.hp.sh.expv3.bb.extension.service.BbAccountExtService;
+import com.hp.sh.expv3.bb.extension.vo.BbAccountVo;
+import com.sun.xml.internal.bind.v2.model.core.Ref;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * @author BaiLiJun  on 2020/2/12
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class BbAccountExtServiceImpl implements BbAccountExtService {
+
+    @Autowired
+    private BbAccountExtMapper bbAccountExtMapper;
+
+
+    @Override
+    public void createBBAccount(Long userId, String asset) {
+        BbAccountVo bbAccountVo = new BbAccountVo();
+        bbAccountVo.setUserId(userId);
+        bbAccountVo.setAsset(asset);
+        bbAccountVo.setBalance(BigDecimal.ZERO);
+        bbAccountVo.setVersion(0L);
+        long now = Instant.now().toEpochMilli();
+        bbAccountVo.setModified(now);
+        bbAccountVo.setCreated(now);
+        bbAccountExtMapper.save(bbAccountVo);
+
+
+    }
+
+    @Override
+    public Boolean bbAccountExist(Long userId, String asset) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("asset", asset);
+        BbAccountVo bbAccountVo = bbAccountExtMapper.queryOne(map);
+        if (bbAccountVo != null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public BbAccountVo getBBAccount(Long userId, String asset) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("asset", asset);
+        BbAccountVo bbAccountVo = bbAccountExtMapper.queryOne(map);
+        return bbAccountVo;
+    }
+}
