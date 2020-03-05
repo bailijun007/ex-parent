6245513ac77ec0aa0573486ee092319164f9c190
wg <java_arch@163.com>
2020-03-05 11:02:57 +0800

    提币状态

--- ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/cash/constant/PaymentStatus.java
+++ ex-fund-account-def/src/main/java/com/hp/sh/expv3/fund/cash/constant/PaymentStatus.java
@@ -9,11 +9,11 @@ public class PaymentStatus {
 	
 	//待查
 	public static final int PENDING = 0;
-	//取消支付
-	public static final int CANCELLED = 1;
 	//支付成功
-	public static final int SUCCESS = 2;
+	public static final int SUCCESS = 1;
 	//支付失败
-	public static final int FAIL = 3;
+	public static final int FAIL = 2;
+	//取消支付
+	public static final int CANCELLED = 3;
 	
 }
