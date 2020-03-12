18bb5b48015b5fd7fe7530463b49650e649862fc
wg <java_arch@163.com>
2020-03-12 21:11:39 +0800

    collector

--- ex-bb-base/src/main/java/com/hp/sh/expv3/bb/component/impl/FeeCollectorSelectorImpl.java
+++ ex-bb-base/src/main/java/com/hp/sh/expv3/bb/component/impl/FeeCollectorSelectorImpl.java
@@ -15,7 +15,7 @@ public class FeeCollectorSelectorImpl implements FeeCollectorSelector {
     //获取手续费收取人账户(这里暂时写死，随机获取一个收取人账户,后期完善)
     @Override
     public Long getFeeCollectorId(Long userId, String asset, String symbol) {
-        Long[] arr = {1111111L, 2222222L};
+        Long[] arr = {1L, 2L};
         int index = (int) (Math.random() * arr.length);
         Long aLong = arr[index];
         return aLong;
--- ex-bb-base/src/main/java/com/hp/sh/expv3/config/db/BBIdConfig.java
+++ ex-bb-base/src/main/java/com/hp/sh/expv3/config/db/BBIdConfig.java
@@ -1,7 +1,6 @@
 package com.hp.sh.expv3.config.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +9,6 @@ import com.hp.sh.expv3.component.id.config.WorkerConfig;
 
 @Configuration
 public class BBIdConfig {
-
-	@Value("${id.generator.dataCenterId}")
-	private int dataCenterId;
-	
-	@Value("${id.generator.serverId}")
-	private int serverId;
 	
 	@Autowired
 	private WorkerConfigBuilder workerConfigBuilder;
