05bacf61c85bbe3c5210626b56065db8522bc15f
xiaobai <18756096010@163.com>
2020-03-16 10:52:34 +0800

    kline 修复历史记录

---
+++ ex-bb-kline/src/main/java/com/hp/sh/expv3/bb/kline/test/TestKline.java
@@ -0,0 +1,27 @@
+package com.hp.sh.expv3.bb.kline.test;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.hp.sh.expv3.bb.kline.vo.BbRepairTradeVo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author BaiLiJun  on 2020/3/16
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestKline {
+
+    @Test
+    public void test(){
+
+    }
+
+}
