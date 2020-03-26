91860bfaebd8092c0e387b760376e00719b8dc69
corleone <535521469@qq.com>
2020-03-26 18:06:39 +0800

    change config

--- ex-pc-match/src/main/resources/start.sh
+++ ex-pc-match/src/main/resources/start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-remote_debug_port=8319
-rmi_server_hostname=18.177.137.233
-jmx_remote_port=8317
+remote_debug_port=8019
+rmi_server_hostname=192.168.0.88
+jmx_remote_port=8017
 config_path=config
 main_class=com.hp.sh.expv3.ExMatchApplication
 
 java \
+-Dagent.instance_uuid=1 \
+-javaagent:/root/hupa/pcmatch/agent/skywalking-agent.jar \
 -cp "libs/*" \
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:HeapDumpPath=~/${remote_debug_port}.hprof \
@@ -18,14 +20,15 @@ java \
 -Dcom.sun.management.jmxremote.ssl=false \
 -Dcom.sun.management.jmxremote.authenticate=false \
 -Dspring.config.location=${config_path}/application.properties \
--Dlogging.config=${config_path}/logback.xml \
+-Dlogging.config=/root/hupa/pcmatch/config/logback.xml \
 -Dpcmatch.id.serverId=1 \
 -Dpcmatch.matchGroupId=1 \
+
+
 ${main_class}
 
-# notice
-# 1. pcmatch.id.serverId
-# 2. pcmatch.matchGroupId
+# -javaagent:/root/hupa/pcmatch/agent/skywalking-agent.jar \
+# java \
 # -cp "libs/*" \
 # -javaagent:agent/skywalking-agent.jar \
 # -Dspring.config.location=config/application.properties \
