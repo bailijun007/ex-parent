72ac6efab5e54fb3b2e3f2f93d910d1cc4ab1ce0
xiaobai <18756096010@163.com>
2020-03-30 19:52:37 +0800

    bb账单接口重构

--- ex-bb-extend-def/src/main/java/com/hp/sh/expv3/bb/extension/constant/BbAccountRecordConst.java
+++ ex-bb-extend-def/src/main/java/com/hp/sh/expv3/bb/extension/constant/BbAccountRecordConst.java
@@ -7,23 +7,6 @@ import java.util.List;
  * @author BaiLiJun  on 2020/3/25
  */
 public class BbAccountRecordConst {
-    //1:买入
-//    public static final int TYPE_BUY_IN =1;
-//    //2:卖出
-//    public static final int TYPE_SELL_OUT =2;
-//
-//    //3.转出至资金账户
-//    public static final int CHANGE_OUT_FUND_ACCOUNT =3;
-//
-//    //4.转出至合约账户
-//    public static final int CHANGE_OUT_PC_ACCOUNT =4;
-//
-//    //5.资金账户转入
-//    public static final int CHANGE_INTO_FUND_ACCOUNT =5;
-//
-//    //6.合约账户转入
-//    public static final int CHANGE_INTO_PC_ACCOUNT =6;
-
     public static final int ACCOUNT_FUND_TO_BB = 3;		//从资金账户转入
 
     public static final int ACCOUNT_BB_TO_FUND = 4;		//转出至资金账户
--- ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/constant/BbExtRedisKey.java
+++
@@ -1,9 +0,0 @@
-package com.hp.sh.expv3.bb.extension.constant;
-
-/**
- * @author BaiLiJun  on 2020/3/4
- */
-public class BbExtRedisKey {
-    public static final String BB_SYMBOL = "bb_symbol";
-
-}
