6a2e0a3cfb6f5beb70a8c1714e2aebd2c0615235
wg <java_arch@163.com>
2020-04-02 14:42:50 +0800

    job

--- ex-pc-base/src/main/java/com/hp/sh/expv3/pc/job/liq/LiqXxlJob.java
+++ ex-pc-base/src/main/java/com/hp/sh/expv3/pc/job/liq/LiqXxlJob.java
@@ -1,13 +1,11 @@
 package com.hp.sh.expv3.pc.job.liq;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.hp.sh.expv3.dev.LimitTimeHandle;
-import com.hp.sh.expv3.utils.DbDateUtils;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.XxlJob;
 
@@ -17,12 +15,7 @@ public class LiqXxlJob {
     
     @Autowired
     private LiquidationService liquidationService;
-    
-    @XxlJob("checkLiqOrder")
-	public void checkLiqOrder() {
-		liquidationService.checkLiqOrder();
-	}
-    
+
 	@XxlJob("checkLiqOrder")
     public ReturnT<String> checkLiqOrder(String param) throws Exception {
 		logger.info("XXL检查强平...，{}", param);
