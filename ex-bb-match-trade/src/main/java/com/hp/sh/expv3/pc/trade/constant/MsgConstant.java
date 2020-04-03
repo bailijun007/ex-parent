34b79db679a22bec423fec5babb362effc7c8ad7
xiaobai <18756096010@163.com>
2020-04-03 09:45:45 +0800

    配置文件修改

--- ex-bb-match-trade/src/main/java/com/hp/sh/expv3/pc/trade/constant/MsgConstant.java
+++ ex-bb-match-trade/src/main/java/com/hp/sh/expv3/pc/trade/constant/MsgConstant.java
@@ -2,11 +2,15 @@ package com.hp.sh.expv3.pc.trade.constant;
 
 import com.hp.sh.expv3.pc.trade.service.impl.SupportBbGroupIdsJobServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author BaiLiJun  on 2020/3/31
  */
 public class MsgConstant {
+
+    @Value("${hp.rocketmq.namespace}")
+    private  static  String namespace;
 
     @Autowired
     private SupportBbGroupIdsJobServiceImpl supportBbGroupIdsJobService;
@@ -14,7 +18,7 @@ public class MsgConstant {
     public static final String TAG_BB_MATCH = "BB_MATCH";
 
     public static final String getMatchTopic(String asset, String symbol){
-        return "exp_v3%bbMatch_"+asset+"__"+symbol;
+        return namespace+"%bbMatch_"+asset+"__"+symbol;
     }
 
 
