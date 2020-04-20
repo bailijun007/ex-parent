4e0dea6cd26b4c7e28a180e4fa34654df8f330f2
xiaobai <18756096010@163.com>
2020-04-20 10:42:10 +0800

    bb账单 添加日志输出

--- ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/api/BbAccountRecordExtApiAction.java
+++ ex-bb-extend/src/main/java/com/hp/sh/expv3/bb/extension/api/BbAccountRecordExtApiAction.java
@@ -11,6 +11,8 @@ import com.hp.sh.expv3.bb.extension.vo.BbAccountRecordVo;
 import com.hp.sh.expv3.bb.extension.vo.BbOrderTradeVo;
 import com.hp.sh.expv3.commons.exception.ExException;
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,8 @@ import java.util.stream.Collectors;
  */
 @RestController
 public class BbAccountRecordExtApiAction implements BbAccountRecordExtApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(BbAccountRecordExtApiAction.class);
 
     @Autowired
     private BbAccountRecordExtService bbAccountRecordExtService;
@@ -42,51 +46,54 @@ public class BbAccountRecordExtApiAction implements BbAccountRecordExtApi {
     @Override
     public List<BbAccountRecordExtVo> query(Long userId, String asset, Integer historyType, Integer tradeType, Long startDate, Long endDate, Integer nextPage, Long lastId, Integer pageSize) {
         this.checkParam(userId, historyType, tradeType, startDate, endDate, nextPage, pageSize);
+        logger.info("userId={},asset={},historyType={},tradeType={},startDate={},endDate={},nextPage={},lastId={},pageSize={}", userId, asset, historyType, tradeType, startDate, endDate, nextPage, lastId, pageSize);
         List<BbAccountRecordExtVo> voList = null;
-        if (null == lastId) {
-            voList = bbAccountRecordExtService.listBbAccountRecords(userId, asset, historyType, tradeType, startDate, endDate, pageSize);
-        } else {
-            voList = bbAccountRecordExtService.listBbAccountRecordsByPage(userId, asset, historyType, tradeType, lastId, nextPage, startDate, endDate, pageSize);
-        }
-
-        if (!CollectionUtils.isEmpty(voList)) {
-            List<Long> refId = new ArrayList<>();
-            for (BbAccountRecordExtVo recordExtVo : voList) {
-                String replaceAll = recordExtVo.getTradeNo().replaceAll("[A-Z]", "");
-                long parseLong = Long.parseLong(replaceAll);
-                refId.add(parseLong);
+        try {
+            if (null == lastId) {
+                voList = bbAccountRecordExtService.listBbAccountRecords(userId, asset, historyType, tradeType, startDate, endDate, pageSize);
+            } else {
+                voList = bbAccountRecordExtService.listBbAccountRecordsByPage(userId, asset, historyType, tradeType, lastId, nextPage, startDate, endDate, pageSize);
             }
+
+            if (!CollectionUtils.isEmpty(voList)) {
+                List<Long> refId = new ArrayList<>();
+                for (BbAccountRecordExtVo recordExtVo : voList) {
+                    String replaceAll = recordExtVo.getTradeNo().replaceAll("[A-Z]", "");
+                    long parseLong = Long.parseLong(replaceAll);
+                    refId.add(parseLong);
+                }
 
 //             Map<Long, List<BbAccountRecordExtVo>> map2 = voList.stream().collect(Collectors.groupingBy(t -> Long.parseLong(t.getTradeNo().replaceAll("[A-Z]", ""))));
-            //根据类型进行分组
-            Map<Integer, List<BbAccountRecordExtVo>> map = voList.stream().collect(Collectors.groupingBy(BbAccountRecordExtVo::getTradeType));
-            List<BbOrderTradeVo> bbOrderTradeVoList = bbOrderTradeExtService.queryByIds(refId);
-            if (map.containsKey(BbAccountRecordConst.TRADE_BUY_IN) || map.containsKey(BbAccountRecordConst.TRADE_SELL_OUT) ||
-                    map.containsKey(BbAccountRecordConst.TRADE_SELL_INCOME) || map.containsKey(BbAccountRecordConst.TRADE_SELL_RELEASE)) {
-                if (!CollectionUtils.isEmpty(bbOrderTradeVoList)) {
-                    Map<Long, BbOrderTradeVo> orderTradeids2Map = bbOrderTradeVoList.stream().collect(Collectors.toMap(BbOrderTradeVo::getId, Function.identity()));
-                    for (BbAccountRecordExtVo recordExtVo : voList) {
-                         long key = Long.parseLong(recordExtVo.getTradeNo().replaceAll("[A-Z]", ""));
-                        if (orderTradeids2Map.containsKey(key)) {
-                            recordExtVo.setFee(orderTradeids2Map.get(key).getFee());
-                        }
-                        //做映射TradeType=9或者11 都属于买入；TradeType=10或者12 都属于卖出
-                        if (recordExtVo.getTradeType().equals(BbAccountRecordConst.TRADE_SELL_INCOME)) {
-                            recordExtVo.setTradeType(BbAccountRecordConst.TRADE_BUY_IN);
-                        }
-                        if (recordExtVo.getTradeType().equals(BbAccountRecordConst.TRADE_SELL_RELEASE)) {
-                            recordExtVo.setTradeType(BbAccountRecordConst.TRADE_SELL_OUT);
+                //根据类型进行分组
+                Map<Integer, List<BbAccountRecordExtVo>> map = voList.stream().collect(Collectors.groupingBy(BbAccountRecordExtVo::getTradeType));
+                List<BbOrderTradeVo> bbOrderTradeVoList = bbOrderTradeExtService.queryByIds(refId);
+                if (map.containsKey(BbAccountRecordConst.TRADE_BUY_IN) || map.containsKey(BbAccountRecordConst.TRADE_SELL_OUT) ||
+                        map.containsKey(BbAccountRecordConst.TRADE_SELL_INCOME) || map.containsKey(BbAccountRecordConst.TRADE_SELL_RELEASE)) {
+                    if (!CollectionUtils.isEmpty(bbOrderTradeVoList)) {
+                        Map<Long, BbOrderTradeVo> orderTradeids2Map = bbOrderTradeVoList.stream().collect(Collectors.toMap(BbOrderTradeVo::getId, Function.identity()));
+                        for (BbAccountRecordExtVo recordExtVo : voList) {
+                            long key = Long.parseLong(recordExtVo.getTradeNo().replaceAll("[A-Z]", ""));
+                            if (orderTradeids2Map.containsKey(key)) {
+                                recordExtVo.setFee(orderTradeids2Map.get(key).getFee());
+                            }
+                            //做映射TradeType=9或者11 都属于买入；TradeType=10或者12 都属于卖出
+                            if (recordExtVo.getTradeType().equals(BbAccountRecordConst.TRADE_SELL_INCOME)) {
+                                recordExtVo.setTradeType(BbAccountRecordConst.TRADE_BUY_IN);
+                            }
+                            if (recordExtVo.getTradeType().equals(BbAccountRecordConst.TRADE_SELL_RELEASE)) {
+                                recordExtVo.setTradeType(BbAccountRecordConst.TRADE_SELL_OUT);
+                            }
                         }
                     }
-                }
-            } else {
-                for (BbAccountRecordExtVo recordExtVo : voList) {
-                    recordExtVo.setFee(recordExtVo.getFee() == null ? BigDecimal.ZERO : recordExtVo.getFee());
+                } else {
+                    for (BbAccountRecordExtVo recordExtVo : voList) {
+                        recordExtVo.setFee(recordExtVo.getFee() == null ? BigDecimal.ZERO : recordExtVo.getFee());
+                    }
                 }
             }
-
+        } catch (Exception e) {
+            logger.error("message={}", e.getMessage());
         }
-
         return voList;
     }
 
