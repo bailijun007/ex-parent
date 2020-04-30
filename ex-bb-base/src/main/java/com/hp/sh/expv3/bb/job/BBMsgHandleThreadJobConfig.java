3ff763291a25be2a44c082ddb891433f5bffc120
wg <java_arch@163.com>
2020-04-30 12:05:28 +0800

    trim

--- ex-bb-base/src/main/java/com/hp/sh/expv3/bb/job/BBMsgHandleThreadJobConfig.java
+++ ex-bb-base/src/main/java/com/hp/sh/expv3/bb/job/BBMsgHandleThreadJobConfig.java
@@ -1,6 +1,5 @@
 package com.hp.sh.expv3.bb.job;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- ex-bb-base/src/main/java/com/hp/sh/expv3/bb/job/MsgShardHandler.java
+++ ex-bb-base/src/main/java/com/hp/sh/expv3/bb/job/MsgShardHandler.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gitee.hupadev.base.exceptions.CommonError;
