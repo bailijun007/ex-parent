ca4aa1422e0fa71a293e3d1bd741d619c7eb1b14
wg <java_arch@163.com>
2020-05-19 15:57:36 +0800

    定时建表

---
+++ ex-pc-base/src/main/java/com/hp/sh/expv3/pc/job/CreateTableJob.java
@@ -0,0 +1,26 @@
+package com.hp.sh.expv3.pc.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.hp.sh.expv3.pc.module.sys.service.ShardTableService;
+
+@Order(Ordered.LOWEST_PRECEDENCE)
+@Component
+public class CreateTableJob {
+    private static final Logger logger = LoggerFactory.getLogger(CreateTableJob.class);
+
+	@Autowired
+	private ShardTableService shardTableService;
+	
+	@Scheduled(cron = "0 0 11 20 * ?")
+	public void handle() {
+		shardTableService.createNextMonthTables();
+	}
+
+}
