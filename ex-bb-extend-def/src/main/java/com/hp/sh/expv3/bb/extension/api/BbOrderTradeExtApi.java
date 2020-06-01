095d3a01c375ed02dd9dd6ee23d1e750c1277c01
xiaobai <18756096010@163.com>
2020-06-01 15:37:00 +0800

    优化

--- ex-bb-extend-def/src/main/java/com/hp/sh/expv3/bb/extension/api/BbOrderTradeExtApi.java
+++ ex-bb-extend-def/src/main/java/com/hp/sh/expv3/bb/extension/api/BbOrderTradeExtApi.java
@@ -83,7 +83,7 @@ public interface BbOrderTradeExtApi {
 
 
     @ApiOperation(value = "获取当前用户交易明细")
-    @GetMapping(value = "/api/extension/bb/orderTrade/queryHistory")
+    @GetMapping(value = "/api/bb/ext/orderTrade/queryHistory")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "用户id", example = "1", required = true),
             @ApiImplicitParam(name = "asset", value = "资产类型", example = "BTC", required = true),
