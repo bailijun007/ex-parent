5c16b5c68f1e09632de0ab0a0003b03dd0fc34fa
xiaobai <18756096010@163.com>
2020-06-05 19:45:08 +0800

    优化

---
+++ query-kline-data-by-third-data/src/main/java/com/hp/sh/expv3/service/impl/NewAssetDefaultPriceService.java
@@ -0,0 +1,63 @@
+package com.hp.sh.expv3.service.impl;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author BaiLiJun  on 2020/6/5
+ */
+@Service
+public class NewAssetDefaultPriceService {
+
+//    @Value("${max.price}")
+    private BigDecimal maxPrice = new BigDecimal(0.145);
+
+//    @Value("${start.price}")
+    private BigDecimal startPrice = new BigDecimal(0.04);
+
+//    @Value("${min.price}")
+    private BigDecimal minPrice = new BigDecimal(0.04);
+
+//    @Value("${stepMax.price}")
+    private BigDecimal stepMaxPrice = new BigDecimal(+0.000015);
+//    @Value("${stepMin.price}")
+    private BigDecimal stepMinPrice = new BigDecimal(-0.000012);
+
+//    @Value("${count}")
+    private int count = 86400;
+
+    public List<BigDecimal> buildPrice() {
+        ArrayList<BigDecimal> price = new ArrayList<>(count);
+        while (true) {
+            BigDecimal nextPrice = nextPrice(stepMaxPrice, stepMinPrice);
+            BigDecimal startPriceNew = startPrice.add(nextPrice);
+            if (startPriceNew.compareTo(minPrice) >= 0 && startPriceNew.compareTo(maxPrice) <= 0) {
+                price.add(startPriceNew);
+                startPrice = startPriceNew;
+            } else {
+                continue;
+            }
+            if (price.size() == count) {
+                break;
+            }
+        }
+
+        return price;
+    }
+
+    public static BigDecimal nextPrice(BigDecimal stepMaxPrice, BigDecimal stepMinPrice) {
+        BigDecimal min = stepMinPrice.min(stepMaxPrice);
+        BigDecimal max = stepMinPrice.max(stepMaxPrice);
+        double width = max.doubleValue() - min.doubleValue();
+        double pDouble = RandomUtils.nextDouble(0, width) + min.doubleValue();
+        return new BigDecimal(pDouble);
+    }
+
+
+
+}
