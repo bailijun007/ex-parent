6344b681de19ce5709f7c64c9d06811aa928ed84
wg <java_arch@163.com>
2020-06-12 18:16:14 +0800

    rpc

--- ex-pj-admin/src/main/java/com/hp/sh/expv3/ExPJAdminApplication.java
+++ ex-pj-admin/src/main/java/com/hp/sh/expv3/ExPJAdminApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@EnableFeignClients({"com.hp.sh.expv3.pc", "com.hp.sh.expv3.bb"})
 @ComponentScan("com.hp.sh.expv3")
 @SpringBootApplication
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
